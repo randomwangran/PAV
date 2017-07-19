92bb3e567bb43c457a9659f69cfadaebee81b10c
Ran Wang <wang1hf@uwindsor.ca>
2017-07-19 11:05:30 -0400

    add warning for the decimal issue

--- perlGlue.pl
+++ perlGlue.pl
@@ -5,12 +5,13 @@
 ## Creator: Ran Wang 
 ## Create Data: <2017-07-17 Mon 15:19>
 ## Modified History:
+## Warning: Becareful with the omission. 
 
 use strict;
 use warnings;
 
 
-for (my $base=1; $base <= 19; $base = $base + 2) {
+for (my $base=0.314; $base <= 2.826; $base = $base + 0.314) {
 
    my $file = "planeZPosition$base.py";
    print "file: $file\n";
@@ -24,7 +25,7 @@ for (my $base=1; $base <= 19; $base = $base + 2) {
     print $fh "#### disable automatic camera reset on 'Show' \n";
     print $fh "paraview.simple._DisableFirstRenderCameraReset() \n";
     print $fh "# create a new 'OpenFOAMReader' \n";
-    print $fh "a6020Dfoam = OpenFOAMReader(FileName='/scratch/orca/superran/Cable-conference/DES/ReynoldNumberPowerOfFive/IP/60-20D/60-20D.foam') \n";
+    print $fh "a6020Dfoam = OpenFOAMReader(FileName='/scratch/orca/superran/Cable-conference/DES/ReynoldNumberPowerOfFive/IP/0/0.foam') \n";
     print $fh "a6020Dfoam.CaseType = 'Reconstructed Case' \n";
     print $fh "a6020Dfoam.Expect64bitLabels = 0 \n";
     print $fh "a6020Dfoam.Createcelltopointfiltereddata = 1 \n";
@@ -47,11 +48,11 @@ for (my $base=1; $base <= 19; $base = $base + 2) {
     print $fh "plotOnIntersectionCurves1 = PlotOnIntersectionCurves(Input=a6020Dfoam) \n";
     print $fh "plotOnIntersectionCurves1.SliceType = 'Plane' \n";
     print $fh "# Properties modified on plotOnIntersectionCurves1.SliceType \n";
-    print $fh "plotOnIntersectionCurves1.SliceType.Origin = [0.0, 0.0, $base.0] \n";
+    print $fh "plotOnIntersectionCurves1.SliceType.Origin = [0.0, 0.0, $base] \n";
     print $fh "plotOnIntersectionCurves1.SliceType.Normal = [0.0, 0.0, 1.0] \n";
     print $fh "# Create a new 'Line Chart View' \n";
     print $fh "# save data \n";
-    print $fh "SaveData('/scratch/orca/superran/Cable-conference/DES/ReynoldNumberPowerOfFive/IP/60-20D/pressure/planeNum$base/20DIP60.csv', proxy=plotOnIntersectionCurves1, Precision=5, \n";
+    print $fh "SaveData('/scratch/orca/superran/Cable-conference/DES/ReynoldNumberPowerOfFive/IP/0/pressure/planeNum$base/IP0.csv', proxy=plotOnIntersectionCurves1, Precision=5, \n";
     print $fh "    UseScientificNotation=0, \n";
     print $fh "    WriteAllTimeSteps=1, \n";
     print $fh "    FieldAssociation='Points') \n";
